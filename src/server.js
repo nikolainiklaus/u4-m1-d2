2db9088b0e37247a8e40f548a2e7aa7ca68659b3
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2023-03-06 15:20:15 +0100

    start script

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ import Express from "express";
 import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./api/authors/index.js";
 import blogPostsRouter from "./api/blogposts/index.js";
+import cors from "cors";
 
 const server = Express();
 const port = 3001;
-
+server.use(cors());
 server.use(Express.json());
 server.use("/authors", authorsRouter);
 server.use("/blogposts", blogPostsRouter);
