05184e19182eabf21c68940e73a71ba044c9f70f
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2023-03-06 15:45:01 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,30 @@ import cors from "cors";
 const server = Express();
 const port = process.env.PORT;
 console.log("port", port);
+
+// const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+
+// server.use(
+//   cors({
+//     origin: (currentOrigin, corsNext) => {
+//       if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+//         // origin is in the whitelist
+//         corsNext(null, true);
+//       } else {
+//         // origin is not in the whitelist
+//         corsNext(
+//           createHttpError(
+//             400,
+//             `Origin ${currentOrigin} is not in the whitelist!`
+//           )
+//         );
+//       }
+//     },
+//   })
+// );
+
 server.use(cors());
+
 server.use(Express.json());
 server.use("/authors", authorsRouter);
 server.use("/blogposts", blogPostsRouter);
