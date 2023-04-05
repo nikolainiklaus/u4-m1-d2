093d14fb7e4763845c63eb7d96d858da2e1692bb
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2023-04-05 15:31:52 +0200

    exclude role from req.author in token

--- src/lib/admin.js
+++ src/lib/admin.js
@@ -1,7 +1,9 @@
 import createHttpError from "http-errors";
+import AuthorsModel from "../api/authors/model.js";
 
-export const adminOnlyMiddleware = (req, res, next) => {
-  if (req.author.role === "Admin") {
+export const adminOnlyMiddleware = async (req, res, next) => {
+  const author = await AuthorsModel.findById(req.author._id);
+  if (author.role === "Admin") {
     next();
   } else {
     next(createHttpError(403, "Admins only endpoint!"));
